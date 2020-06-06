36e51972a58c0bf1bee6a1bd105f1cff0c11eb80
samama-tariq <samama177@gmail.com>
2020-06-06 13:07:49 +0500

    chapter-2 task done

---
+++ Chapter-2/Task-4/app.js
@@ -0,0 +1,6 @@
+var name="Muhammad Samama";
+var age=20;
+var course="Certified Mobile application development";
+alert(name);
+alert(age+" years old");
+alert(course);
---
+++ Chapter-2/Task-6/app.js
@@ -0,0 +1,2 @@
+var email="samama177@gmail.com";
+alert("My email address is"+" "+email);
