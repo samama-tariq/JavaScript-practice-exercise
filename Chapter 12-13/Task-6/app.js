cfffa531a71908ebb1376d53daf245a036db3849
samama-tariq <samama177@gmail.com>
2020-06-09 14:54:51 +0500

    Chapter 12-13 task done

---
+++ Chapter 12-13/Task-6/app.js
@@ -0,0 +1,8 @@
+// var greeting;
+// var hour = 13;
+// if (hour < 18){
+//     greeting = "Good day";
+// }
+// else{
+//   greeting = "Good evening"; 
+// } now the code will work fine (the opening bracket in else bloack is missing in original code)
